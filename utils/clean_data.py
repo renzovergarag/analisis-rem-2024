1ce47d94bd6c54fbdd600ecc4cd78551256ea9e4
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-12-17 14:23:40 -0300

    in progress: se agrega archivo de configuracion de vs code y se actualiza la funcion de limpieza de datos del percapita

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -2,9 +2,9 @@ def clean_data_percapita(df):
     # Filtrar filas en función de las columnas: 'GENERO', 'TRAMO'
     # df = df[(df['GENERO'].str.contains("HOMBRE", regex=False, na=False, case=False)) & (df['TRAMO'].str.contains("A", regex=False, na=False, case=False))]
     # Seleccionar columnas: 'RUT', 'FECHA_NACIMIENTO' y 4 otras columnas
-    df = df.loc[:, ['RUT', 'FECHA_NACIMIENTO', 'GENERO', 'TRAMO', 'NOMBRE_CENTRO', 'MOTIVO']]
+    df = df.loc[:, ['FECHA_CORTE', 'NOMBRE_CENTRO', 'EDAD', 'GENERO', 'MOTIVO', 'CODIGO_CMV', 'Recuento']]
     # Se ha realizado 1 agregación agrupados en columnas: 'NOMBRE_CENTRO', 'GENERO'
-    df = df.groupby(['NOMBRE_CENTRO']).agg(RUT_count=('RUT', 'count')).reset_index()
+    df = df.groupby(['NOMBRE_CENTRO']).agg(CENTRO_count=('NOMBRE_CENTRO', 'count')).reset_index()
     # Ordenar por columna: 'RUT_count' (descendente)
-    df = df.sort_values(['RUT_count'], ascending=[False])
+    df = df.sort_values(['CENTRO_count'], ascending=[False])
     return df
